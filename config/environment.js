dba85777da6b79794129522059083d0415dfd633
AllenEsteves <estevesallen@yahoo.com>
2020-09-25 02:29:38 +0800

    first commit

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,19 @@ module.exports = function(environment) {
     environment,
     rootURL: '/',
     locationType: 'auto',
+    firebase: {
+      apiKey: "AIzaSyCkxFXQX3F8WEZqZTNblZti-1WzlqRsSGo",
+      authDomain: "login-demo-23d27.firebaseapp.com",
+      databaseURL: "https://login-demo-23d27.firebaseio.com",
+      projectId: "login-demo-23d27",
+      storageBucket: "login-demo-23d27.appspot.com",
+      messagingSenderId: "1082889640894",
+      appId: "1:1082889640894:web:7ae52b669268d9b754b7ef",
+      measurementId: "G-2JBX96JFKX" 
+    },
+    torii: {
+      sessionServiceName: 'session'
+     },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
